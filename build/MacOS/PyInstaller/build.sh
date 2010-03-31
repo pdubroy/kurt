23872a121aadc32e64fec8014de728038df3b124
Patrick Dubroy <pdubroy@gmail.com>
2010-03-30 22:44:11 -0400

    Now builds for 10.5. Added instructions and also the stub executable object files

--- build/MacOS/PyInstaller/build.sh
+++ build/MacOS/PyInstaller/build.sh
@@ -4,5 +4,5 @@ echo Running PyInstaller...
 python pyinstaller/Build.py Kurt/kurt.spec
 patch Kurt.app/Contents/Info.plist < Info.plist.patch
 cp -rv Kurt/dist/kurt/* Kurt.app/Contents/MacOS
-cp -rv /opt/local/Library/Frameworks/QtGui.framework/Versions/Current/Resources/qt_menu.nib Kurt.app/Contents/Resources
+cp -rv /usr/local/Trolltech/Qt-4.6.2/lib/QtGui.framework/Versions/Current/Resources/qt_menu.nib Kurt.app/Contents/Resources
 echo Build complete.
