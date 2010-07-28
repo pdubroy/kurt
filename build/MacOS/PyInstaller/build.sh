dba077976f1c4f3346cd3f8fb98815d98613ed13
Patrick Dubroy <pdubroy@gmail.com>
2010-07-27 21:16:52 -0400

    Add missing images for find bar

--- build/MacOS/PyInstaller/build.sh
+++ build/MacOS/PyInstaller/build.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
+echo Setting up PyInstaller...
+PREV_DIR=`pwd`
+cd pyinstaller/source/linux
+python ./Make.py
+make
+cd $PREV_DIR
+python pyinstaller/Configure.py
+echo Done PyInstaller setup.
+
+echo Generating build spec...
+python pyinstaller/Makespec.py --out=Kurt ../../../src/kurt.py
+patch Kurt/kurt.spec < kurt.spec.patch
+echo Done generating build spec.
+
 echo Running PyInstaller...
 python pyinstaller/Build.py Kurt/kurt.spec
 patch Kurt.app/Contents/Info.plist < Info.plist.patch
